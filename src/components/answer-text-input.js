a6657fc0cdabcacc2c1bd7a22ca0138a9dc4ceca
Marty Powell <jpowel25@gmail.com>
2018-01-03 09:56:59 -0500

    fixed but with the check eligibility button being enabled when nothing was in the input

--- src/components/answer-text-input.js
+++ src/components/answer-text-input.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
 
 class TextInputAnswerComponent extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = this.getInitialState();
+	}
+
+	getInitialState = () => {
+		return {
+			inputValue: ''
+		};
+	};
+
+	setInputValue = (onInputChangeEvent) => {
+		const { message } = this.props;
+
+		this.setState({
+			inputValue: onInputChangeEvent.target.value
+		});
+
+		this.props.handlers.onHandleTextInputChange(message.key, message.validationTypes, onInputChangeEvent);
+	}
+	
     render() {
 		const { message, handlers, validationErrors } = this.props;
         return (
@@ -10,11 +32,11 @@ class TextInputAnswerComponent extends Component {
 						<input 
 							type='text' 	 
 							id={message.key}
-							onChange={(e) => handlers.onHandleTextInputChange(message.key, message.validationTypes, e)} />
+							onChange={(e) => this.setInputValue(e) } />
 						<button 
 							className="btn contentButton"
 							type="submit"
-							disabled={validationErrors.length}
+							disabled={!this.state.inputValue || validationErrors.length}
 							onClick={() => handlers.onButtonSelect(message.id, message.key)}>Check Eligibility</button>
 					</form>
 				</div>
