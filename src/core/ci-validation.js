625b6930ce38b220c66485cb9336e63fe5ebb884
Marty Powell <jpowel25@gmail.com>
2018-01-03 15:24:56 -0500

    fixed param names and validation messages

--- src/core/ci-validation.js
+++ src/core/ci-validation.js
@@ -4,23 +4,23 @@ class CiValidation {
             test: function(name) {
                 return /^[a-zA-Z ]+$/.test(name);
             },
-            message: "Please specify your First and Last name separated by a space. Ex. \"Dan Fox\""
+            message: "Please specify your first and last name separated by a space. Ex. \"Dan Fox\""
         },
         "address": {
-            test: function(name) {
-                return /^[a-zA-Z0-9\s,'-]*$/.test(name);
+            test: function(address) {
+                return /^[a-zA-Z0-9\s,'-]*$/.test(address);
             },
-            message: "Please specify your First and Last name separated by a space. Ex. \"400 Washington Ave Towson, MD 21204\""
+            message: "Please specify your address. Ex. \"400 Washington Ave Towson, MD 21204\""
         },
         "email": {
-            test: function(name) {
-                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(name);
+            test: function(email) {
+                return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
             },
             message: "Please specify a valid email address. Ex. \"bot@baltimorecountymd.gov\""
 		},
 		"zipCode": {
-            test: function(name) {
-                return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(name);
+            test: function(zipCode) {
+                return /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(zipCode);
             },
             message: "Please specify a valid zipcode. Ex. \"21204\""
         }
