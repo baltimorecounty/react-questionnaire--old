8a64262f10fa185a4343d78be8666d4a6f95f94a
Marty Powell <jpowel25@gmail.com>
2018-01-12 08:50:11 -0500

    step-3-1

--- sample.questionnaire.js
+++ sample.questionnaire.js
@@ -6,15 +6,19 @@ var bcQuestionnaire = {
 			key: "hadProblemWithSample",
 			fieldType: "radio",
 			options: {
-			true: {
-				text: "Yes",
-				nextStep: 2
-			},
-			false: {
-				text: "No",
-				nextStep: 3
+				true: {
+					text: "Yes",
+					nextStep: 2
+				},
+				false: {
+					text: "No",
+					nextStep: 3
+				}
 			}
-			}
+		},
+		{
+			id: 3,
+			text: "Thanks for taking the time to help us out! Come back soon. :smile:",
 		}
 	]
 };
